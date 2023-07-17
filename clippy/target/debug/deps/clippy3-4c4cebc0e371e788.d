6817ae8e16ca96be61ba27a5d5348c9ec9175bf6
AndreyTorkhov <124518996+AndreyTorkhov@users.noreply.github.com>
2023-07-17 13:19:03 +0300

    Add files via upload

---
+++ clippy/target/debug/deps/clippy3-4c4cebc0e371e788.d
@@ -0,0 +1,8 @@
+/home/andre_root/rustlings/exercises/clippy/target/debug/deps/clippy3-4c4cebc0e371e788.rmeta: clippy3.rs Cargo.toml
+
+/home/andre_root/rustlings/exercises/clippy/target/debug/deps/clippy3-4c4cebc0e371e788.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
